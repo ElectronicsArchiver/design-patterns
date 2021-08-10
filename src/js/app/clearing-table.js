6c27192f46de9eae30549b6e10d59553b8fa0c9e
Nick Pearson <nick.pearson@york.ac.uk>
2021-08-10 09:01:13 +0100

    Updates hotline opening times + clearing only toggle fix

--- src/js/app/clearing-table.js
+++ src/js/app/clearing-table.js
@@ -375,7 +375,7 @@ define(['jquery', 'app/searchables', 'app/utils', 'app/modal-link'],
           var checked = false;
 
           if( window.localStorage ) {
-            checked = ( localStorage.getItem( 'course-search-in-clearing-only' ) !== 'false' );
+            checked = ( localStorage.getItem( 'course-search-in-clearing-only' ) == true );
           }
 
           var showAllCoursesButton = $( '#showAllCourses' );
@@ -958,7 +958,8 @@ define(['jquery', 'app/searchables', 'app/utils', 'app/modal-link'],
           '<p>Opening times:</p>' +
           '<ul>' +
             '<li>Tuesday 10 August: 8.30am - 6pm BST</li>' +
-            '<li>Wednesday 11 - Friday 13 August: 8am - 6pm BST</li>' +
+            '<li>Wednesday 11: 8am - 6pm BST</li>' +
+            '<li>Thursday 12 - Friday 13 August: 8am - 5pm BST</li>' +
           '</ul>' +
           '<h3>Before you call</h3>' +
           '<ol>' +
